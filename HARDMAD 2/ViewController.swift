a3bf2eedc927a237922059b9149402b769822cc7
IAmGlebKorotkov <korotkovgleb200@gmail.com>
2025-02-19 02:23:41 +0700

    Initial Commit

---
+++ HARDMAD 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HARDMAD 2
+//
+//  Created by Gleb Korotkov on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
